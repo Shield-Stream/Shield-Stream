c919c0c4e500ebcbd150c63d7d6d2b7988fd3f9d
luciano-mesquita <lucianomesquita01@gmail.com>
2024-05-30 10:38:40 -0300

    Ajuste Logo e Responsivo

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -61,16 +61,16 @@ export default function MobileMenu() {
       >
         <ul className="bg-gray-800 px-4 py-2">
           <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
-              Sign in
+            <Link href="/" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
+              Home
             </Link>
           </li>
           <li>
             <Link
-              href="/signup"
+              href="https://api.whatsapp.com/send?phone=555555555555&amp;text=Ol%C3%A1%2C%20gostaria%20de%20um%20usu%C3%A1rio%20teste." target="_blank"
               className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
             >
-              Sign up
+              Contato
             </Link>
           </li>
         </ul>
